982c19e804725d442a57f4fd71b23211f8a0ff1c
terez4 <terezamorodev@gmail.com>
2023-03-05 21:22:27 -0300

    Adding samples to library on script file

---
+++ script.js
@@ -0,0 +1,21 @@
+const SAMPLE_LIB = {
+    'Horn' : [
+        {note: 'A#', octave: 2, file: 'Samples/horns-sus-ff-a#2-PB-loop.wav'},
+        {note: 'A#', octave: 3, file: 'Samples/horns-sus-ff-a#3-PB-loop.wav'},
+        {note: "A#", octave: 4, file: 'Samples/horns-sus-ff-a#4-PB-loop.wav'},
+        {note: 'C#', octave: 3, file: 'Samples/horns-sus-ff-c#3-PB-loop.wav'},
+        {note: 'C#', octave: 4, file: 'Samples/horns-sus-ff-c#4-PB-loop.wav'},
+        {note: 'C#', octave: 5, file: 'Samples/horns-sus-ff-c#5-PB-loop.wav'},
+        {note: 'E', octave: 2, file: 'Samples/horns-sus-ff-e2-PB-loop.wav'},
+        {note: 'E', octave: 3, file: 'Samples/horns-sus-ff-e3-PB-loop.wav'},
+        {note: 'E', octave: 4, file: 'Samples/horns-sus-ff-e4-PB-loop.wav'},
+        {note: 'E', octave: 5, file: 'Samples/horns-sus-ff-e5-PB-loop.wav'},
+        {note: 'G', octave: 2, file: 'Samples/horns-sus-ff-g2-PB-loop.wav'},
+        {note: 'G', octave: 3, file: 'Samples/horns-sus-ff-g3-PB-loop.wav'},
+        {note: 'G', octave: 4, file: 'Samples/horns-sus-ff-g4-PB-loop.wav'}
+    ]
+};
+
+function getSample(instrument, noteAndOctave) {
+
+}
